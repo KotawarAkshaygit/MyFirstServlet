7446e2164e5684878fc93db18341957bcd0aa0f9
Kotawar Akshay <akshaykumarak5g4@gmail.com>
2019-08-23 15:53:56 +0530

    insert
     Initial commit
    :wq

---
+++ src/com/isteer/www/Servlet1.java
@@ -0,0 +1,26 @@
+package com.isteer.www;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/add")
+public class Servlet1 extends HttpServlet {
+	
+public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException
+{
+	int i=Integer.parseInt(request.getParameter("a"));
+	int j=Integer.parseInt(request.getParameter("b"));
+	
+	int k= i+j;
+	
+	PrintWriter pw=response.getWriter();
+	pw.print(k);
+}
+}
